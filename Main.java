f0154c1764bb8397ef43488fb88ed57076be3bc6
Tharun <mygithubupdate@gmail.com>
2021-01-09 08:08:17 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(Strings[] args){
+    String fname;
+    String lname;
+    Scanner myScanObj = new Scanner(System.in);
+    fname = myScanObj.nextLine();
+    lname = myScanObj.nextLine();
+    fullName = fname + " " + lname;
+    System.out.println("hi " fullName);
+  }
+}
