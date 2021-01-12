7732298af511450c178f6ebebfd63b8d180cb22d
Tharun <mygithubupdate@gmail.com>
2021-01-12 17:19:36 +0000

    Update Main.java
    More encapsulation

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-
 public class Main{
   public static void main(Strings[] args){
     String fname;
     String lname;
-    Scanner myScanObj = new Scanner(System.in);
-    fname = myScanObj.nextLine();
-    lname = myScanObj.nextLine();
+    fname = Scan.nextLine();
+    lname = Scan.nextLine();
     fullName = fname + " " + lname;
     System.out.println("hi " fullName);
   }
