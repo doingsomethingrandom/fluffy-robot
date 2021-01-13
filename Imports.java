3538f2ed6b3c2484e940f0b26cd2d841a0d9b76c
Tharun <mygithubupdate@gmail.com>
2021-01-13 09:47:45 +0000

    Update Imports.java

--- Imports.java
+++ Imports.java
@@ -3,6 +3,6 @@ import java.util.Date;
 abstract class Imports{
   public static main(Strings[] args){
     Scanner scan = new Scanner(System.in);
-    Date date = new Date();
+    Date date = new Date(int date, int month, int day);
   }
 }
